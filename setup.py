27c519c084c333a9adef783bbcfdda11d15c9b25
Diego W. Antunes <devlware@gmail.com>
2011-04-03 19:45:44 -0300

    several updates in files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name="Ontime",
       version="1.0",
       description="Application to download schedule for public bus lines in Curitiba",
-      license="maybe MIT",
+      license="MIT",
       author="Diego W. Antunes",
       author_email="devlware@gmail.com",
       url="http://github.com/devlware/Ontime",
